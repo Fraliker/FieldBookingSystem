ab670a1c0cf49c91f760f2ed8aeb52e58c4f7d5d
hisham02 <hisham02@gmail.com>
2017-05-23 12:54:00 -0700

    added static files, changed port number to 8080

--- server/src/App.ts
+++ server/src/App.ts
@@ -33,7 +33,7 @@ class App {
     this.express.use(logger('dev'));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
-    this.express.use('/', express.static(__dirname+'/pages'));
+    this.express.use('/', express.static(__dirname+'/static_files'));
 
 
   }
